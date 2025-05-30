7e0bf7c3ad40eb69f33d6394ce109e7cd1aa2b92
diogocrespi <diogocrespi@alunos.utfpr.edu.br>
2025-05-30 17:34:39 +0000

    lib

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Viora',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -29,8 +29,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),      home: const MyHomePage(title: 'Viora Home Page'),
     );
   }
 }
